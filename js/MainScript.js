af6d1fc4acafff03d85f15330f11cd261c9daa29
Omar Taher Saad <omartahersaad@outlook.com>
2019-09-09 01:43:26 +0200

    Fix A4 size & Update to Bootstrap 4.3

--- js/MainScript.js
+++ js/MainScript.js
@@ -1,13 +1,5 @@
 $(document).ready(function () {
 	$("html, body").animate({scrollTop: 0}, 1000, 'swing');
-	//Add .active to current navbar button
-	var url = window.location.href;
-	if (url.includes('?')) {
-		url = url.substr(0,url.indexOf('?'));
-	}
-	$('ul.navbar-nav a').filter(function() {
-		 return this.href == url;
-	}).addClass("active").closest(".nav-item").addClass("active");
 	//Dynamic Sticky footer
 	$("body > div.container-fluid").first().css("min-height","calc(100vh - "+($("nav").outerHeight()+$("footer").outerHeight())+"px)");
 });
